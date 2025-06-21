dc5c6da68f92c750ca01f431926931380dcb9ceb
Yash <vyash7265@gmail.com>
2025-06-22 02:02:46 +0530

    fixes deployment error

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import path from "path"
 import dbConnect from "./lib/dbconnect.js";
 import { app, server } from "./lib/socket.js";
 import authRoutes from "./routes/auth.route.js"
@@ -10,6 +11,7 @@ import messageRoute from "./routes/message.route.js";
 dotenv.config()
 
 const port=process.env.PORT;
+const __dirname=path.resolve();
 
 app.use(cors({
     origin:process.env.FRONTEND_URL,
@@ -23,6 +25,14 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoute);
 
+if(process.env.NODE_END==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
+app.use()
 server.listen(port,()=>{
     console.log(`Server is listening at port ${port}`);
     dbConnect();
