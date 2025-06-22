c503cb702ccd9bd9247e356d1fe298e2f1092e9b
YashVerma <146102449+YashVerma-code@users.noreply.github.com>
2025-06-22 09:36:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl=import.meta.env.VITE_BACKEND_BASE_URL ||"/";
+const backendUrl=import.meta.env.VITE_BACKEND_BASE_URL ||"https://gupshup-mychat-app.vercel.app/";
 console.log("Base URL : ",backendUrl);
 export const axiosInstance=axios.create({
     baseURL:backendUrl,
